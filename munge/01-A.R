c2760c7794a9c00ca3c67b37f6977524fbfcee9f
Thomas Richardson <thomasrichardson@newhuaweiapstudent-10-58-206-41.ncl.ac.uk>
2021-11-22 16:40:06 +0000

    Merged enrolment files and slected only the learners

--- munge/01-A.R
+++ munge/01-A.R
@@ -1,1 +1,16 @@
-# Example preprocessing script.
+# Merge all enrolment files
+enrolments = merge(cyber.security.1_enrolments,
+                   merge(cyber.security.2_enrolments,
+                         merge(cyber.security.3_enrolments,
+                               merge(cyber.security.4_enrolments,
+                                     merge(cyber.security.5_enrolments,
+                                           merge(cyber.security.6_enrolments,
+                                                 cyber.security.7_enrolments,
+                                                 all=TRUE),
+                                           all=TRUE),
+                                     all=TRUE),
+                               all=TRUE),
+                         all=TRUE),
+                   all=TRUE)
+# Select only individuals with the role "learner"
+enrolments = enrolments[enrolments$role == "learner", ]
