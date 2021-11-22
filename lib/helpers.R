4fd23f21aaad6b387abbd357a5d0aadf9f1b9120
Thomas Richardson <thomasrichardson@newhuaweiapstudent-10-58-206-41.ncl.ac.uk>
2021-11-22 16:44:17 +0000

    Created get_season helper function to return the season of a date

--- lib/helpers.R
+++ lib/helpers.R
@@ -1,4 +1,15 @@
-helper.function <- function()
-{
-  return(1)
+# get_season(date)
+# Helper function to return the season of a date
+get_season = function(date){
+  if (month(date) <= 2 | (month(date) <= 3 & day(date) <= 19)) {
+    return("Winter")
+  } else if (month(date) <= 5 | (month(date) <= 6 & day(date) <= 20)) {
+    return("Spring")
+  } else if (month(date) <= 8 | (month(date) <= 9 & day(date) <= 21)) {
+    return("Summer")
+  } else if (month(date) <= 11 | (month(date) <= 12 & day(date) <= 20)) {
+    return("Autumn")
+  } else {
+    return("Winter")
+  }
 }
