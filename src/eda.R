9dfc8fcdb7b28d6191caeb68e8ae451194d91e0b
thomasr981 <thomasr981@yahoo.co.uk>
2021-11-23 14:33:35 +0000

    Plotted bar chart of enrolments in each month

--- src/eda.R
+++ src/eda.R
@@ -1,8 +1,11 @@
 library('ProjectTemplate')
 load.project()
 
-for (dataset in project.info$data)
-{
-  message(paste('Showing top 5 rows of', dataset))
-  print(head(get(dataset)))
-}
+# Plot enrolments in each month as a bar chart
+months_plot = ggplot(months, aes(x = as_date(date), y = n)) +
+  geom_bar(stat = "identity") +
+  scale_x_date(date_breaks = "1 month", labels = date_format("%m-%Y")) +
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1),
+        plot.title = element_text(hjust = 0.5)) +
+  labs(x="Month", y="Number of enrolments") +
+  ggtitle("Number of enrolments in each month")
