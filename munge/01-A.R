afcb4ea784409537016d5fdcf2ed044ec0f055fd
thomasr981 <thomasr981@yahoo.co.uk>
2021-11-25 14:58:34 +0000

    Rearrange some code from the first two munge files to ensure chronology

--- munge/01-A.R
+++ munge/01-A.R
@@ -15,14 +15,6 @@ enrolments = merge(cyber.security.1_enrolments,
 # Select only individuals with the role "learner"
 enrolments = enrolments[enrolments$role == "learner", ]
 
-# Set up placeholder vector for season column
-season = rep(0, nrow(enrolments))
-# Use get_season to assign each enrolment to the correct season
-for (i in 1:nrow(enrolments)) {
-  season[i] = get_season(enrolments$enrolled_at[i])
-}
-enrolments = data.frame(enrolments, season)
-
 # Count number of enrolments in each month and store this in a new tibble
 months <- enrolments %>% mutate(enrolled_at = as_datetime(enrolled_at),
                         enrolled_month = month(enrolled_at),
--- munge/02-A.R
+++ munge/02-A.R
@@ -1,3 +1,11 @@
+# Set up placeholder vector for season column
+season = rep(0, nrow(enrolments))
+# Use get_season to assign each enrolment to the correct season
+for (i in 1:nrow(enrolments)) {
+  season[i] = get_season(enrolments$enrolled_at[i])
+}
+enrolments = data.frame(enrolments, season)
+
 # Count number of enrolments in each season across a two year period
 seasons <- filter(enrolments, ( as_date("2016-05-01")
                                 <= enrolled_at & enrolled_at
